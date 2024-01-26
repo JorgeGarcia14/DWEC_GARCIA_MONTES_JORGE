ee841365b07911d81ed6e7c309016fcc2914c6b7
Jorge Garcia <jorge.garmon12@gmail.com>
2024-01-26 10:24:17 +0100

    Validacion Formulario

---
+++ Ejercicios2EV/Formularios/validacionFormulario.js
@@ -0,0 +1,77 @@
+window.onload = iniciar;
+
+function iniciar() {
+    document.getElementById("enviar").addEventListener("click", validar, false);
+}
+
+function validarNombre() {
+    var elemento = document.getElementById("nombre");
+    if (!elemento.checkValidity()) {
+        if(elemento.validity.valueMissing){
+            error2(elemento, "El " + elemento.id + " no puede estar vacío") 
+        }
+        if (elemento.validity.patternMismatch){
+            error2(elemento, "El " + elemento.id + " debe contener entre 2 y 15 caracteres")
+        }
+       
+        return false;
+    }
+    borrarError();
+    return true;
+}
+
+function validarTelefono() {
+    var elemento = document.getElementById("telefono");
+    if (!elemento.checkValidity()) {
+        if(elemento.validity.valueMissing){
+            error2(elemento, "El " + elemento.id + " no puede estar vacío") 
+        }
+        if (elemento.validity.patternMismatch){
+            error2(elemento, "El " + elemento.id + " debe contener entre 9 digitos")
+        }
+        
+        return false;
+    }
+    borrarError();
+    return true;
+}
+
+function validarEdad() {
+    var elemento = document.getElementById("edad");
+    if (!elemento.checkValidity()) {
+        if(elemento.validity.valueMissing){
+            error2(elemento, "El " + elemento.id + " no puede estar vacío") 
+        }
+        if (elemento.validity.rangeOverflow){
+            error2(elemento, "El " + elemento.id + " debe ser menor de 100 años")
+        }
+        if (elemento.validity.rangeUnderflow){
+            error2(elemento, "El " + elemento.id + " debe ser mayor de 18 años")
+        }
+        return false;
+    }
+    borrarError();
+    return true;
+}
+
+function validar(e) {
+    if (validarNombre() && validarEdad() && validarTelefono() && confirm("¿Estás seguro que quieres enviar el formulario?")) {
+        return true;
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
+
+function borrarError(){
+    var formulario = document.forms[0];
+    for (let i =0; i< formulario.length; i++){
+        formulario.elements[i].className="";
+    }
+}
+
+function error2(elemento, mensaje){
+    document.getElementById("mensajeError").innerHTML = mensaje;
+    elemento.className = "error";
+    elemento.focus();
+}
