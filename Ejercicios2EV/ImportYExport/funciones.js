7db5bd94b624be77a0d52b0c1ba0f0eed5b01402
Jorge Garcia <jorge.garmon12@gmail.com>
2024-02-07 09:42:14 +0100

    Import Export añadido

---
+++ Ejercicios2EV/ImportYExport/funciones.js
@@ -0,0 +1,9 @@
+// Exportar un único elemento
+export const cadenaMayuscula = str => str.toUpperCase();
+
+const NUMEROPI =3.14;
+const NUMEROE =2.71;
+const NUMEROCERO =0.00;
+
+//Exportar más de un elemento simultáneamentee
+export{NUMEROPI, NUMEROE, NUMEROCERO};
---
+++ Ejercicios2EV/ImportYExport/modulos.js
@@ -0,0 +1,5 @@
+import { cadenaMayuscula } from "./funciones.js";
+const saludo = cadenaMayuscula("hola, bienvenida!")
+console.log(saludo)
+import { NUMEROCERO, NUMEROE, NUMEROPI } from "./funciones.js";
+console.log(`LOS NUMEROS SON ${NUMEROCERO}, ${NUMEROE} Y ${NUMEROPI}`);
