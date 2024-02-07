ad0b537ba9accf193e59dda6219755c2e9dfd861
Jorge Garcia <jorge.garmon12@gmail.com>
2024-02-07 11:53:50 +0100

    202 ACABADA (1 REVISION MAS Y ENTREGA)

--- 202JGM.js
+++ 202JGM.js
@@ -1,8 +1,19 @@
 window.addEventListener("load", inicio);
 
         function inicio() {
+
+            document.getElementById("miForm").addEventListener("submit", validar); //FORMULARIO
+
+            document.getElementById("nombre").addEventListener("input", validarNombre); //NOMBRE
+
+            document.getElementById("dni").addEventListener('input', validarDNI);
             
-            document.getElementById("mensaje").addEventListener("input", contadorCaracteres);
+            let arrayFechas = document.querySelectorAll("input[name='fecha']");
+            arrayFechas.forEach(function (fecha){
+                fecha.addEventListener("input", validarFecha)
+            }); // FECHA
+
+            document.getElementById("mensaje").addEventListener("input", validarMensaje);
 
             document.getElementById("seleccionarTodos").addEventListener("change", seleccionarTodo);
 
@@ -11,45 +22,38 @@ window.addEventListener("load", inicio);
             let checkboxes = document.querySelectorAll("input[type='checkbox']");
 
             checkboxes.forEach(function (checkbox) {
-                checkbox.addEventListener("change", validacionCheckbox);
+                checkbox.addEventListener("change", validarCheckbox);
             });
 
-            document.getElementById("botonGoogle").addEventListener("click", function () 
-            { 
-                window.location.href = "https://www.google.com"; 
-            });
+            document.getElementById("botonGoogle").addEventListener("click", irAGoogle);
 
-            document.getElementById('dni').addEventListener('input', function() {
-                var dni = this.value; // Obtener el valor del campo de entrada del DNI
-                var valido = validarDNI(dni); // Validar el DNI
-                
-                if (valido) {
-                    document.getElementById("validoDNI").innerHTML ="DNI válido";
-                    document.getElementById("errorDNI").innerHTML ="";
-                    // Aquí puedes realizar cualquier acción adicional si el DNI es válido
-                } else {
-                    document.getElementById("errorDNI").innerHTML ="DNI no válido";
-                    document.getElementById("validoDNI").innerHTML ="";
-                    // Aquí puedes realizar cualquier acción adicional si el DNI es inválido
-                }
-            });
-
-            document.getElementById('nombre').addEventListener('input', function() {
-                if (this.checkValidity()) {
-                  document.getElementById("validoNombre").innerHTML="Nombre válido";
-                  document.getElementById("errorNombre").innerHTML="";
-                } else {
-                    document.getElementById("validoNombre").innerHTML="";
-                    document.getElementById("errorNombre").innerHTML="Nombre no válido";
-                }
-              });
+        }
             
-              document.getElementById('fechaMatriculacionDia').addEventListener('input', validarFecha);
-              document.getElementById('fechaMatriculacionMes').addEventListener('input', validarFecha);
-              document.getElementById('fechaMatriculacionAnio').addEventListener('input', validarFecha);
-          }
+        function validar(){
+            if(validarDNI(document.getElementById("dni")) && validarFecha() && validacionCheckbox() && validarNombre() && validarMensaje()){
+                alert("Formulario enviado con exito");
+            }
+            else{
+                alert("Formulario no enviado, contiene errores");
+            }
+        }
+
+        function validarDNI(){
+            var dni = document.getElementById("dni").value; // Obtener el valor del campo de entrada del DNI
+            var valido = comprobarDNI(dni); // Validar el DNI
             
-        
+            if (valido) {
+                document.getElementById("validoDNI").innerHTML ="DNI válido";
+                document.getElementById("errorDNI").innerHTML ="";
+                return true;
+                // Aquí puedes realizar cualquier acción adicional si el DNI es válido
+            } else {
+                document.getElementById("errorDNI").innerHTML ="DNI no válido";
+                document.getElementById("validoDNI").innerHTML ="";
+                return false;
+                // Aquí puedes realizar cualquier acción adicional si el DNI es inválido
+            }
+        }
 
         function validarFecha() {
             var dia = document.getElementById('fechaMatriculacionDia');
@@ -57,58 +61,94 @@ window.addEventListener("load", inicio);
             var año = document.getElementById('fechaMatriculacionAnio');
         
             // Verificar si los valores son válidos según los patrones
-            if (dia.checkValidity()) {
-              document.getElementById("validoDia").innerHTML = "Dia válido";
-              document.getElementById("errorDia").innerHTML = "";
-              // Aquí puedes realizar cualquier acción adicional si la fecha es válida
-            } else {
-                document.getElementById("errorDia").innerHTML = "Dia no válido";
-                document.getElementById("validoDia").innerHTML = "";
-              // Aquí puedes realizar cualquier acción adicional si la fecha es inválida
-            }
-            if (mes.checkValidity()) {
-                document.getElementById("validoMes").innerHTML = "Mes válido";
-                document.getElementById("errorMes").innerHTML = "";
-                // Aquí puedes realizar cualquier acción adicional si la fecha es válida
-              } else {
-                  document.getElementById("errorMes").innerHTML = "Mes no válido";
-                  document.getElementById("validoMes").innerHTML = "";
-                // Aquí puedes realizar cualquier acción adicional si la fecha es inválida
-              }
-              if (año.checkValidity()) {
-                document.getElementById("validoAño").innerHTML = "Año válido";
-                document.getElementById("errorAño").innerHTML = "";
-                // Aquí puedes realizar cualquier acción adicional si la fecha es válida
-              } else {
-                  document.getElementById("errorAño").innerHTML = "Año no válido";
-                  document.getElementById("validoAño").innerHTML = "";
-                // Aquí puedes realizar cualquier acción adicional si la fecha es inválida
-              }
+            if (dia.checkValidity() && mes.checkValidity() && año.checkValidity()) {
+              document.getElementById("validoFecha").innerHTML = "Fecha correcta";
+              document.getElementById("errorFecha").innerHTML = "";
+              return true;
           }
+          else{
+            document.getElementById("validoFecha").innerHTML = "";
+            document.getElementById("errorFecha").innerHTML = "Fecha incorrecta";
+            return false;
+          }
+        }
 
-        function validarDNI(dni) {
-            var numero;
-            var letra;
-            var letraCorrecta;
-            var expresion_regular_dni = /^[XYZ]?\d{5,8}[A-Z]$/;
-        
-            dni = dni.toUpperCase(); // Aseguramos que el DNI esté en mayúsculas
-        
-            if(expresion_regular_dni.test(dni) === true) {
-                numero = dni.substr(0, dni.length-1);
-                numero = numero.replace('X', 0);
-                numero = numero.replace('Y', 1);
-                numero = numero.replace('Z', 2);
-                letra = dni.substr(dni.length-1, 1);
-                numero = numero % 23;
-                letraCorrecta = 'TRWAGMYFPDXBNJZSQVHLCKET';
-                letraCorrecta = letraCorrecta.substring(numero, numero+1);
-                return letraCorrecta === letra; // Devolvemos directamente el resultado de la comparación
+        function comprobarDNI(dni) {
+                    var numero;
+                    var letra;
+                    var letraCorrecta;
+                    var expresion_regular_dni = /^[XYZ]?\d{5,8}[A-Z]$/;
+                
+                    dni = dni.toUpperCase(); // Aseguramos que el DNI esté en mayúsculas
+                
+                    if(expresion_regular_dni.test(dni) === true) {
+                        numero = dni.substr(0, dni.length-1);
+                        numero = numero.replace('X', 0);
+                        numero = numero.replace('Y', 1);
+                        numero = numero.replace('Z', 2);
+                        letra = dni.substr(dni.length-1, 1);
+                        numero = numero % 23;
+                        letraCorrecta = 'TRWAGMYFPDXBNJZSQVHLCKET';
+                        letraCorrecta = letraCorrecta.substring(numero, numero+1);
+                        return letraCorrecta === letra; // Devolvemos directamente el resultado de la comparación
+                    } else {
+                        return false;
+                    }
+        }
+
+        function seleccionarTodo() {
+            let seleccionador = document.getElementById("seleccionarTodos");
+            let arrayDias = document.querySelectorAll("input[name=dia]");
+            let mensajeSeleccionador = document.getElementById("mensajeSeleccionador");
+
+
+            if (seleccionador.checked) {
+                // Marcar todos los checkboxes
+                for (let i = 0; i < arrayDias.length; i++) {
+                    arrayDias[i].checked = true;
+                }
+                mensajeSeleccionador.innerHTML = "Deseleccionar todo";
             } else {
-                return false;
+                // Desmarcar todos los checkboxes
+                for (let i = 0; i < arrayDias.length; i++) {
+                    arrayDias[i].checked = false;
+                }
+                mensajeSeleccionador.innerHTML = "Seleccionar todo";
+            }
+
+        }
+
+        function validarCheckbox() {
+            let arrayDias = document.querySelectorAll("input[type=checkbox]");
+            let mensajeErrorCheckbox = document.getElementById("mensajeErrCheckbox");
+            let checkboxesMarcados = 0;
+
+            for (let i = 0; i < arrayDias.length; i++) {
+                if (arrayDias[i].checked) {
+                    checkboxesMarcados++;
+                }
+            }
+
+            if (checkboxesMarcados < 2) {
+                mensajeErrorCheckbox.innerHTML = "Debes marcar al menos dos opciones";
+            } else {
+                mensajeErrorCheckbox.innerHTML = "";
             }
         }
 
+        function validarNombre(){
+            document.getElementById('nombre').addEventListener('input', function() {
+                if (this.checkValidity()) {
+                  document.getElementById("validoNombre").innerHTML="Nombre válido";
+                  document.getElementById("errorNombre").innerHTML="";
+                  return true;
+                } else {
+                    document.getElementById("validoNombre").innerHTML="";
+                    document.getElementById("errorNombre").innerHTML="Nombre no válido";
+                    return false;
+                }
+              });
+        }
 
         function añadirCurso() {
 
@@ -151,56 +191,25 @@ window.addEventListener("load", inicio);
             });
         }
 
-
-        function validacionCheckbox() {
-            let arrayDias = document.querySelectorAll("input[type=checkbox]");
-            let mensajeErrorCheckbox = document.getElementById("mensajeErrCheckbox");
-            let checkboxesMarcados = 0;
-
-            for (let i = 0; i < arrayDias.length; i++) {
-                if (arrayDias[i].checked) {
-                    checkboxesMarcados++;
-                }
-            }
-
-            if (checkboxesMarcados < 2) {
-                mensajeErrorCheckbox.innerHTML = "Debes marcar al menos dos opciones";
-            } else {
-                mensajeErrorCheckbox.innerHTML = "";
-            }
-        }
-
-
-        function seleccionarTodo() {
-            let seleccionador = document.getElementById("seleccionarTodos");
-            let arrayDias = document.querySelectorAll("input[name=dia]");
-            let mensajeSeleccionador = document.getElementById("mensajeSeleccionador");
-
-
-            if (seleccionador.checked) {
-                // Marcar todos los checkboxes
-                for (let i = 0; i < arrayDias.length; i++) {
-                    arrayDias[i].checked = true;
-                }
-                mensajeSeleccionador.innerHTML = "Deseleccionar todo";
-            } else {
-                // Desmarcar todos los checkboxes
-                for (let i = 0; i < arrayDias.length; i++) {
-                    arrayDias[i].checked = false;
-                }
-                mensajeSeleccionador.innerHTML = "Seleccionar todo";
-            }
-
-        }
-
-
-
-        function contadorCaracteres() {
+        function validarMensaje() {
             let mensaje = document.getElementById("mensaje");
             let contador = document.getElementById("contador");
+            let mensajeError = document.getElementById("errorMensaje");
             const longitudMax = mensaje.getAttribute("maxlength");
-            contador.innerHTML = `Caracteres restantes: ${longitudMax - mensaje.value.length}`;
-            if (mensaje.value.length == 0) {
-                contador.innerHTML = "";
+
+            if(!mensaje.checkValidity()){
+                mensajeError.innerHTML="Mensaje no valido";
+                contador.innerHTML="";
+                return false;
             }
+            else{
+               contador.innerHTML = `Caracteres restantes: ${longitudMax - mensaje.value.length}`;
+                mensajeError.innerHTML="";
+                return true;
+            }
+            
+        }
+
+        function irAGoogle(){
+            window.location.href = "https://www.google.com"; 
         }
